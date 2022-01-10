35c17dc02720e0d0fba99c4810f2e435d6b18e6b
riadh <riadh.farhati@allocab.com>
2022-01-10 09:57:22 +0100

    build adnrodi

---
+++ app.config.js
@@ -0,0 +1,30 @@
+
+export default {
+    name: process.env.APP_NAME,
+    slug: 'my-app-ci-cd-expo',
+    version: '0.0.1',
+    owner: 'riadh',
+    orientation: 'portrait',
+    icon: "./assets/icon.png",
+    scheme: 'allocabdriver',
+    userInterfaceStyle: 'automatic',
+    splash: {
+      "image": "./assets/splash.png",
+      backgroundColor: '#333',
+    },
+  
+    extra: {
+
+      APP_ID: "com.test.riadh",
+      APP_NAME: "my-app-ci-cd-expo",
+      // Config variables
+   
+    },
+    android: {
+        package: "com.test.riadh"
+      },
+    plugins: [],
+    hooks: {
+      postPublish: [  ],
+    },
+  };
